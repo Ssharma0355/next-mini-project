9e550629ce9c0e61dca26a3590f9a9e3f8e2e5b6
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-07 16:07:44 +0530

    header-footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <h1>Footer</h1>
+      <h1 className="text-center bg-dark text-light">Footer</h1>
     </div>
-  )
+  );
 }
 
 export default Footer
